ddf8656b996a2ceae39f2f6c7b016afe068ff123
FichteFoll <fichtefoll2@googlemail.com>
2018-10-11 21:51:01 +0200

    Use parameter unpacking and update more defs

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const S = require('./string')
-module.exports = function TimeStamps(dispatch){
+module.exports = function TimeStamps(dispatch) {
     const blocked = new Set()
 
-    dispatch.hook('S_ADD_BLOCKED_USER', 1, block)
-    dispatch.hook('S_USER_BLOCK_LIST', 1, (event) => {
-        event.blockList.forEach(block)
-    })
-    dispatch.hook('C_REMOVE_BLOCKED_USER', 1, (event) => {
-        blocked.delete(event.name)
-    })
-    dispatch.hook('S_LOGIN', 1, (event) => {
-        blocked.clear()
-    })
-    function block(user){
+    function block(user) {
         blocked.add(user.name)
     }
-    function processChatEvent(event){
+
+    function processChatEvent(event) {
         if(event.channel === 26) return
         if(blocked.has(event.authorName)) return false
         var time = new Date()
@@ -23,6 +14,17 @@ module.exports = function TimeStamps(dispatch){
         event.authorName = `</a>${timeStr}][<a href='asfunction:chatNameAction,${event.authorName}@0@0'>${event.authorName}</a>`
         return true
     }
+
+    dispatch.hook('S_ADD_BLOCKED_USER', 2, block)
+    dispatch.hook('S_USER_BLOCK_LIST', 2, ({blockList}) => {
+        blockList.forEach(block)
+    })
+    dispatch.hook('C_REMOVE_BLOCKED_USER', 1, ({name}) => {
+        blocked.delete(name)
+    })
+    dispatch.hook('S_LOGIN', 'raw', () => {
+        blocked.clear()
+    })
     dispatch.hook('S_CHAT', 2, processChatEvent)
     dispatch.hook('S_PRIVATE_CHAT', 1, processChatEvent)
 }
